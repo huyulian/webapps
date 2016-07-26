628755ed125e332b446a699de007c01af2d517d6
胡玉莲 <hyl_4221@126.com>
2016-07-26 09:50:29 +0800

    ddfdf

---
+++ one/js/js.js
@@ -0,0 +1,17 @@
+
+
+function show() {
+	var popup=document.getElementById("popup");
+	var mask=document.getElementById("mask");
+	popup.style.display="block";
+	mask.style.display="block";
+
+}
+
+function exit() {
+	var popup=document.getElementById("popup");
+	var mask=document.getElementById("mask");
+	popup.style.display="none";
+	mask.style.display="none";
+	
+}
